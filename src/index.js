0f1763f628f4f110f54c801cd1e35624809708c9
Raphael Melo de Lima <raphaelmelolima@gmail.com>
2020-07-01 13:07:08 -0300

    set cors headers

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const usersController = require('./controllers/users');
 
+const port = 8000;
 const app = express();
 app.use(bodyParser.json());
-const port = 8000;
 
 app.listen(port, () => {
     console.log(`Servidor ligado. Acesse em  http://localhost:${port}`);
 });
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 // Endpoints
 app.post('/api/create-user', usersController.createUser);
